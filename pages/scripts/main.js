c28596aa89027134bb98f92db4af084f96f70971
Yuta Hayashi <yuta.hayashi96@gmail.com>
2022-06-23 19:48:02 +0900

    main.jsの作成とconstの追加

---
+++ pages/scripts/main.js
@@ -0,0 +1,44 @@
+const omikujiConfig = {
+    "ゾウ": {
+        aboutTemplate: [
+            'ゾウテンプレート1',
+            'ゾウテンプレート2',
+            'ゾウテンプレート3',
+            'ゾウテンプレート4'
+        ],
+        luckyAnimal: ['luckyAnimalぞう1','luckyAnimalぞう2','luckyAnimalぞう3','luckyAnimalぞう4'],
+        luckyAction: ['luckyActionぞう1','luckyActionぞう2','luckyActionぞう3','luckyActionぞう4'],
+        luckyFood: ['luckyFoodぞう1','luckyFoodぞう2','luckyFoodぞう3','luckyFoodぞう4'],
+    },
+    "イヌ": {
+        aboutTemplate: [
+            'イヌテンプレート1',
+            'イヌテンプレート2',
+            'イヌテンプレート3',
+            'イヌテンプレート4'
+        ],
+        luckyAnimal: ['luckyAnimalイヌ1','luckyAnimalイヌ2','luckyAnimalイヌ3','luckyAnimalイヌ4'],
+        luckyAction: ['luckyActionイヌ1','luckyActionイヌ2','luckyActionイヌ3','luckyActionイヌ4'],
+        luckyFood: ['luckyFoodイヌ1','luckyFoodイヌ2','luckyFoodイヌ3','luckyFoodイヌ4'],
+    },
+    "ネコ": {
+        aboutTemplate: [
+            'ネコテンプレート1',
+            'ネコテンプレート2',
+            'ネコテンプレート3',
+            'ネコテンプレート4'],
+        luckyAnimal: ['luckyAnimalネコ1','luckyAnimalネコ2','luckyAnimalネコ3','luckyAnimalネコ4'],
+        luckyAction: ['luckyActionネコ1','luckyActionネコ2','luckyActionネコ3','luckyActionネコ4'],
+        luckyFood: ['luckyFoodネコ1','luckyFoodネコ2','luckyFoodネコ3','luckyFoodネコ4'],
+    },
+    "キリン": {
+        aboutTemplate: [
+            'キリンテンプレート1',
+            'キリンテンプレート2',
+            'キリンテンプレート3',
+            'キリンテンプレート4'],
+        luckyAnimal: ['luckyAnimalキリン1','luckyAnimalキリン2','luckyAnimalキリン3','luckyAnimalキリン4'],
+        luckyAction: ['luckyActionキリン1','luckyActionキリン2','luckyActionキリン3','luckyActionキリン4'],
+        luckyFood: ['luckyFoodキリン1','luckyFoodキリン2','luckyFoodキリン3','luckyFoodキリン4'],
+    },
+};
