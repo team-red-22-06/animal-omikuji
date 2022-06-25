45e3f3a28c218445e0dbae8742953636ac9a3d85
ttakuma17 <ttanimiya@gmail.com>
2022-06-25 18:53:57 +0900

    動物占い用にフォルダ整理

--- animal-omikuji/scripts/index.js
+++ animal-omikuji/scripts/index.js
@@ -1,4 +1,4 @@
-const animalPageArr = ["neko.html", "panda.html", "saru.html", "zou.html"];
+const animalPageArr = ["neko.html", "panda.html", "saru.html", "zou.html", "inu.html", "buta.html"];
 let i = Math.floor(Math.random() * animalPageArr.length);
 const animalPage = document.getElementById("randomPage");
 const buttonClick = () => {
