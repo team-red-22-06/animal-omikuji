b1c7f95c1400d6f643a29f836b645b384b3229d2
ttakuma17 <ttanimiya@gmail.com>
2022-06-26 13:35:13 +0900

    TOPページ、動物占いページのJS表示

--- animal-pages/scripts/index.js
+++
@@ -1,6 +0,0 @@
-const animalPageArr = ["neko.html", "panda.html", "saru.html", "zou.html", "inu.html", "buta.html"];
-let i = Math.floor(Math.random() * animalPageArr.length);
-const animalPage = document.getElementById("randomPage");
-const buttonClick = () => {
-	animalPage.setAttribute("href", animalPageArr[i]);
-}
--- animal-pages/scripts/main.js
+++ animal-pages/scripts/main.js
@@ -1,6 +1,6 @@
 const omikujiConfig = {
     "ゾウ": {
-        animalImg: '/pages/images/animals/zou.png',
+        animalImg: './images/animals/zou.png',
         aboutTemplate: [
             '君は動物園のスター！恥ずかしがり屋でも色んな友達に会ってみよう',
             '毎日人を背中に乗せてる君、たまには家でゆっくりするんだゾウ',
@@ -12,43 +12,43 @@ const omikujiConfig = {
         luckyFood: ['リンゴ', 'ナシ', 'モモ', 'さくらんぼ'],
     },
     "ブタ": {
-        animalImg: '/pages/images/animals/buta.png',
+        animalImg: './images/animals/buta.png',
         aboutTemplate: [
             '良くも悪くも普通の日！今日は頑張りすぎず、のんびりと過ごしましょう。',
             '友達と遊ぶと吉なようです！友達がいなくても、今日偶然出会った人を誘って楽しみましょう。',
             '好きなものに夢中になってみたい一日。あなたのペースを乱すものは、どんなものでも今日は距離を置きましょう。',
             '甘いものを食べると、リラックスして異性と接することができそう。好きな人とスイーツを分け合っては？',
         ],
-        luckyAnimal: ['犬。。じゃなくて猫','カラス','バッタ','ネズミ'],
-        luckyAction: ['猿回し','帽子を被る','食事','日向ぼっこ'],
-        luckyFood: ['バナナ','カレーライス','ササ','うまい棒'],
+        luckyAnimal: ['犬。。じゃなくて猫', 'カラス', 'バッタ', 'ネズミ'],
+        luckyAction: ['猿回し', '帽子を被る', '食事', '日向ぼっこ'],
+        luckyFood: ['バナナ', 'カレーライス', 'ササ', 'うまい棒'],
     },
     "ネコ": {
-        animalImg: '/pages/images/animals/neko.png',
+        animalImg: './images/animals/neko.png',
         aboutTemplate: [
             '最近のんびりしがちなあなたは少し早めの行動が大切な一日。遅刻は許されませんよ。',
             '今日のあなたは超ラッキーな一日。どんなことがあっても神の使いがあなたを守ってくれることでしょう。',
             '自分のメンテナンスに使うお金は、新たな金運を呼び込む様子。思い切って休養を。',
             '小さな幸せで心がいっぱいに満たされそう。好きな趣味や読書などに夢中になると、昼間のことをすっかり忘れて幸せな気持ちで眠れますよ。',
         ],
-        luckyAnimal: ['犬','狼','ちょうちょ','コバエ'],
-        luckyAction: ['お手','泥浴び','川下り','月に向かって吠える'],
-        luckyFood: ['ビーフジャーキー','干草','リンゴ','牛丼'],
+        luckyAnimal: ['犬', '狼', 'ちょうちょ', 'コバエ'],
+        luckyAction: ['お手', '泥浴び', '川下り', '月に向かって吠える'],
+        luckyFood: ['ビーフジャーキー', '干草', 'リンゴ', '牛丼'],
     },
     "ヒツジ": {
-        animalImg: '/pages/images/animals/hituji.png',
+        animalImg: './images/animals/hituji.png',
         aboutTemplate: [
             '今日のあなたは超アンラッキーな一日。残念ながら頭が動物になってしまいます',
             '今日のあなたには休憩が必要なようです。一日美味しいものを食べて、ゆっくりしましょう！',
             '人から勧められた本や映画の中に、心をくすぐられるような特別なものがあるでしょう。早速おススメを聞いてみましょう！',
             '誰かのまねをしたり、気取ったりせず、自分らしさを大切にして、周囲の異性と接してください。今日は気になるあの人とうまく話せるはずです。',
         ],
-        luckyAnimal: ['トンビ','ハト','たぬき','きつね'],
-        luckyAction: [,'昼寝','弱肉強食','毛づくろい','ボスに従う'],
-        luckyFood: [,'寿司','ラーメン','のり弁','アイスクリーム'],
+        luckyAnimal: ['トンビ', 'ハト', 'たぬき', 'きつね'],
+        luckyAction: [, '昼寝', '弱肉強食', '毛づくろい', 'ボスに従う'],
+        luckyFood: [, '寿司', 'ラーメン', 'のり弁', 'アイスクリーム'],
     },
     "イヌ": {
-        animalImg: '/pages/images/animals/inu.png',
+        animalImg: './images/animals/inu.png',
         aboutTemplate: [
             '今日のあなたはブラック系の洋服を身につけることで運気が上昇します。早速お出かけしましょう',
             '偶然乗り合わせたエレベーターや乗り物などで、意外な異性との出逢いがありそうな一日です。羨ましい。',
@@ -60,7 +60,7 @@ const omikujiConfig = {
         luckyFood: ['カップ麺', 'ラムネ', 'フォー', 'クッキー'],
     },
     "カバ": {
-        animalImg: '/pages/images/animals/kaba.png',
+        animalImg: './images/animals/kaba.png',
         aboutTemplate: [
             '新たな恋愛を育む未来が見えます。恋人がいない方は、特に今日という日を重要視して行動するようにしてください。',
             'あなたの職場でちょっとしたトラブルが持ち上がるかもしれません。そんな時は動物のように我関せず態度で逃げましょう',
@@ -72,7 +72,7 @@ const omikujiConfig = {
         luckyFood: ['紅茶', '和菓子', 'インドカレー', '団子'],
     },
     "コアラ": {
-        animalImg: '/pages/images/animals/koara.png',
+        animalImg: './images/animals/koara.png',
         aboutTemplate: [
             '恋愛運が向上している予感...。ですが、どんだけ頑張っても今日のところは単なる出逢いだけで留まります',
             '財運が好調な一日。宝くじを買いに行くことをお勧めします。当たったらご連絡くださいね。',
@@ -84,7 +84,7 @@ const omikujiConfig = {
         luckyFood: ['グミ', '炭酸水', 'ホットケーキ', 'たこ焼き'],
     },
     "クマ": {
-        animalImg: '/pages/images/animals/kuma.png',
+        animalImg: './images/animals/kuma.png',
         aboutTemplate: [
             '仕事でもプライベートでも毎日のようにこなさなくてはいけないことが、なんとなく面倒だと感じる日。今日は一日休みましょう。',
             '異性に対して不信感をだいてしまいそう。優しい言葉をかけてもらっても、裏を考えてしまって素直に喜べないようです。',
@@ -96,7 +96,7 @@ const omikujiConfig = {
         luckyFood: ['うどん', 'そば', '天丼', 'たくあん'],
     },
     "パンダ": {
-        animalImg: '/pages/images/animals/panda.png',
+        animalImg: './images/animals/panda.png',
         aboutTemplate: [
             '直感を大事にしたい日。人や物に対するこれまでの固定観念を一度手放して、自分がどう感じるかに注目しましょう。',
             '生きる喜びに満ちた一日になりそうです。ご飯がおいしいと感じたり、音楽を聴いて感動したり、そんな生活の中にちりばめられた小さな幸せを再確認する日になるでしょう。',
@@ -108,7 +108,7 @@ const omikujiConfig = {
         luckyFood: ['チョコ', 'せんべい', 'パン', 'ナン'],
     },
     "ペンギン": {
-        animalImg: '/pages/images/animals/pengin.png',
+        animalImg: './images/animals/pengin.png',
         aboutTemplate: [
             '感受性がとても優れている日。自分が感じたことを優先して、物事を判断してください。もしピンとこないなら、やめておくのが正解です。',
             '恋愛運はかなり波乱含みです。好きな人に対して、今まで我慢していたことやヤキモチで爆発しそうになるかもしれません。',
@@ -120,7 +120,7 @@ const omikujiConfig = {
         luckyFood: ['からし', 'わさび', '甘酒', '焼き芋'],
     },
     "ライオン": {
-        animalImg: '/pages/images/animals/raion.png',
+        animalImg: './images/animals/raion.png',
         aboutTemplate: [
             '友人とのおしゃべりなどよりも、部屋を整えることを優先したい日です。汚れている場所を探してみましょう。',
             '人気運が上昇して、行く先々で声がかかりそうです。異性からの誘いもあるでしょう。',
@@ -132,7 +132,7 @@ const omikujiConfig = {
         luckyFood: ['トマト', 'アイスキャンディー', 'コカ・コーラ', 'するめ'],
     },
     "リス": {
-        animalImg: '/pages/images/animals/risu.png',
+        animalImg: './images/animals/risu.png',
         aboutTemplate: [
             '良い物を人と分かち合える一日。周囲にもっと仲良くなりたいと感じる人がいたら、あなたのお気に入りのアイテムをプレゼントしてみると効果的です。',
             '思っていたことが異性に伝わるでしょう。まさに以心伝心。意中の相手から電話やメールがくるかもしれません。',
@@ -178,7 +178,7 @@ class Omikuji {
     getDisplayInfo() {
         // 返り値例：
         // {
-        //     animalImg: '/pages/images/animals/zou.png',
+        //     animalImg: '/images/animals/zou.png',
         //     aboutTemplate: 'ゾウテンプレート3',
         //     luckyAnimal: 'luckyAnimalぞう3',
         //     luckyAction: 'luckyActionぞう1',
@@ -193,4 +193,128 @@ class Omikuji {
         }
         return animalDict
     }
+
+    getOmikujiHtml() {
+        return `
+        <div class="scrollview p-2">
+            <div class="container red-double-border-box my-3 py-3">
+                <div class="d-flex ustify-content-center align-items-center flex-column">
+                    <p class="h5 text-danger">動物占いみくじ</p>
+                    <hr class="border-red my-3">
+                    <img src="${this.getDisplayInfo().animalImg}" width="270px">
+                    <h1 class="text-danger text-big">${this.yourAnimal}</h1>
+                    <hr class="border-red my-3">
+                    <p class="text-danger m-2">${this.getDisplayInfo().aboutTemplate}</p>
+                    <hr class="border-red my-3">
+                    <p class="text-danger text-sakura m-2">ラッキー動物</p>
+                    <h5 class="text-danger">${this.getDisplayInfo().luckyAction}</h5>
+                    <hr class="border-red my-3">
+                    <p class="text-danger text-sakura m-2">ラッキーアクション</p>
+                    <h5 class="text-danger">${this.getDisplayInfo().luckyAction}</h5>
+                    <hr class="border-red my-3">
+                    <p class="text-danger text-sakura m-2">ラッキーフード</p>
+                    <h5 class="text-danger">${this.getDisplayInfo().luckyFood}</h5>
+                    <hr class="border-red my-3">
+                    <button class="btn btn-danger rounded-pill my-3" onclick="showTop()">もう一度おみくじを引く</button>
+                </div>
+            </div>
+        </div>
+        `;
+    }
 }
+
+let omikuji = new Omikuji();
+console.log(omikuji.getKeyFromDict(omikujiConfig));
+// console.log(omikuji.getAnimal());
+
+// TOPページを表示する時の関数
+function showTop() {
+    let top = document.getElementById("top-page");
+    const topHTML = `
+            <div>
+            <div class="text-center">
+                <h1 class="text-danger font-syuji">ソフトウェア開発</h1>
+                <h1 class="text-danger font-syuji">みくじ</h1>
+                <img class="light m-4 omikuji-img" src="./images/omikuji.png" />
+            </div>
+            <div class="d-flex justify-content-around align-items-center pt-3 pb-5">
+                <button type="button" class="btn btn-danger rounded-pill font-syuji" onclick="showOmikuji()">
+                    おみくじを引く
+                </button>
+            </div>
+        </div>
+        <div class="d-flex flex-column">
+            <div class="d-flex justify-content-center align-items-center">
+                <a href="about.html">
+                    <p class="text-danger font-syuji middle-font">このおみくじについて</p>
+                </a>
+                <p class="text-danger middle-font">｜</p>
+                <a href="developers.html">
+                    <p class="text-danger font-syuji text-decoration-none middle-font">開発者一覧</p>
+                </a>
+            </div>
+            <div class="text-center">
+                <p class="footer-font font-syuji text-danger">
+                    ©ソフトウェア開発みくじ
+                </p>
+            </div>
+        </div>
+    `;
+
+    top.innerHTML = topHTML;
+    top.classList.add(
+        "vh-100",
+        "d-flex",
+        "justify-content-center",
+        "align-items-center",
+        "flex-column"
+    );
+
+    let omikuji = document.getElementById("omikuji-page");
+    omikuji.classList.remove(
+        "vh-100",
+        "d-flex",
+        "justify-content-center",
+        "align-items-center",
+        "flex-column"
+    );
+    omikuji.classList.add("d-none");
+    omikuji.innerHTML = "";
+
+    return top;
+}
+
+// おみくじを表示する時の関数
+function showOmikuji() {
+    let omikujiEle = document.getElementById("omikuji-page");
+
+    omikujiEle.classList.add(
+        "vh-100",
+        "d-flex",
+        "justify-content-center",
+        "align-items-center",
+        "flex-column",
+        "d-block"
+    );
+
+    // 呼び出し方を検討
+    // let i = Math.floor(Math.random() * omikujiList.length);
+    // getDisplayInfo
+    omikujiEle.innerHTML = omikuji.getOmikujiHtml();
+
+    // TOPページはおみくじページの表示の際には不要
+    let top = document.getElementById("top-page");
+    top.classList.remove(
+        "vh-100",
+        "d-flex",
+        "justify-content-center",
+        "align-items-center",
+        "flex-column"
+    )
+    top.innerHTML = "";
+
+    return omikujiEle;
+}
+
+// 初期はTOPページを表示
+showTop();
